7ce86f729d666d45ba832a56dbd6907efdb99067
harald42 <harald@the-develop.net>
2012-11-30 11:18:17 +0100

    delete identical files

--- build/coretemplate/etc-fs20/init.d/S09cpbootenv.sh
+++
@@ -1,9 +0,0 @@
-#-------------------------------------------
-#! /bin/sh
-# GPIO Kernel module
-  echo -n "copy ubootenv to tmp/uenv... ";
-  /sbin/ubootenv -d >> /tmp/uenv
-  echo "done";
-
-#end
-#--------------------------------------------
--- build/coretemplate/etc-fs20/init.d/S09setrelay.sh
+++
@@ -1,9 +0,0 @@
-#-------------------------------------------
-#! /bin/sh
-# GPIO Kernel module
-  echo -n "set relay states... ";
-  /sbin/setrelay.sh
-  echo "done";
-
-#end
-#--------------------------------------------
