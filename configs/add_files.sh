79c07653331280b35b2e75584777da3b94ebda38
harald42 <harald@the-develop.net>
2013-01-29 09:43:39 +0100

    Commit message

--- configs/add_files.sh
+++ configs/add_files.sh
@@ -54,10 +54,10 @@ if [  ${CONFIG_X2} ]
     echo "add files X2"
     cp -a "${DEL_PROJ_DIR}/build/coretemplate/etc-x2/."  "${ROOTFS_DIR}/etc"
     cp -a "${DEL_PROJ_DIR}/build/coretemplate/sbin-x2/."  "${ROOTFS_DIR}/sbin"
-    cp  "${DEL_PROJ_DIR}/build/coretemplate/apps-x2/libcoap/examples/coap-client"  "${ROOTFS_DIR}/bin/coap-client"
-    cp -a "${DEL_PROJ_DIR}/build/coretemplate/apps-x2/bird/bird/bird"  "${ROOTFS_DIR}/sbin"
+    cp  "${DEL_PROJ_DIR}/build/coretemplate/lib-x2/libcoap/examples/coap-client"  "${ROOTFS_DIR}/bin/coap-client"
     webgen -d "${DEL_PROJ_DIR}/build/coretemplate/webgen/Control-X2/" run
     cp -a "${DEL_PROJ_DIR}/build/coretemplate/webgen/Control-X2/output/."  "${ROOTFS_DIR}/usr/share/www"
+    cp -a "${DEL_PROJ_DIR}/build/coretemplate/cgi-bin-x2/."  "${ROOTFS_DIR}/usr/share/www/cgi-bin"
 fi
 #endif
 exit 0
